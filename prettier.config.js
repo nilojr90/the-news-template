8d55f9fd9874657092249f02f103a3c0fe6c73fe
Nilson Ramos de Menezes Júnior <nilojr90@gmail.com>
2021-02-13 14:01:48 -0300

    Configuração inicial do projeto.

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // Força utilização de aspas simples
+  trailingComma: 'all', // Força utilização da virgula no fim de objetos
+  arrowParens: 'always', // Força utilização de parênteses com funções de um único parâmetro.
+}
